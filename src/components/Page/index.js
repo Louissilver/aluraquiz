baba43c83a7355508832d6cf985cebc68af0a2ea
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-01-25 23:18:23 -0300

    tentativa 3

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -6,14 +6,13 @@ const Page = () => (
     <NextSeo
       title="Sidekick Quiz"
       description="Quiz do ajudante de herói."
-      canonical="https://sidekickquiz.vercel.app/"
       openGraph={{
-        url: `${db.bg}`,
+        url: "https://sidekickquiz.vercel.app/",
         title: 'Sidekick Quiz',
         description: 'Quiz do ajudante de herói.',
         images: [
           {
-            url: 'https://www.example.ie/og-image-01.jpg',
+            url: `${db.bg}`,
             width: 800,
             height: 600,
           },
